8ca5fab4ccd43077843fdb490e0ad0fa85478298
kamral <48605965+kamral@users.noreply.github.com>
2020-10-07 19:49:39 +0300

    Update settings.py
    # Heroku: Update database configuration from $DATABASE_URL.

--- Django_WebApp/settings.py
+++ Django_WebApp/settings.py
@@ -136,7 +136,3 @@ LOGIN_URL='login'
 LOGOUT_REDIRECT_URL='/'
 
 # Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
